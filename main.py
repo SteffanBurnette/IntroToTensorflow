766b89fcc23f67b71335e224a0d06629ba9a12f1
Steffan Burnette <burnettesteffansep7@gmail.com>
2023-11-22 15:19:50 -0500

    model training testing

--- main.py
+++ main.py
@@ -178,3 +178,4 @@ linear_est.train(train_input_fn)  # train
 result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on testing data
 
 print(result['accuracy'])  # the result variable is simply a dict of stats about our model
+
